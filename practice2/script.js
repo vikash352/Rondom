76dd11f7852d6b51f125fc93ab4d397d097b0bb7
vikash352 <vs1789332@gmail.com>
2024-10-10 22:10:42 +0530

    first commit

--- practice2/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("mousemove",(details)=>{
-    var rect=document.querySelector("#rect")
-    var xval=gsap.utils.mapRange(0,window.innerWidth,200+rect.getBoundingClientRect().width/2,window.innerWidth-(200+rect.getBoundingClientRect().width/2),details.clientX
-)
-    gsap.to("#rect",{
-        left:xval,ease:Power3,
-    })
-})
